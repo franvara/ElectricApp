e359a84720745eddd6d9185dcfc3a78656f84de2
franvara <franvaracruz@gmail.com>
2015-01-28 19:53:22 +0100

    barra lateral

---
+++ app/src/main/java/com/fran/electricroutes/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.fran.electricroutes;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/* Section Home fragment */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.home, container, false);
+
+        return rootView;
+    }
+}
